ed38bfeb0ccd0c3a2986662102748b06fb4c3714
Francisco Zambrano <fzambran@bancolombia.com.co>
2021-12-28 22:07:28 -0500

    feat: learning general variable types
    Francisco Zambrano

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+const val PI = 3.1416
+fun main() {
+    println("Hello World!")
+    var money = 23
+    println("money - $money")
+    money = 10
+    println("money - $money")
+    val name = "Francisco"
+    println("name - $name")
+    println("PI - $PI")
+    val firstValue = 10
+    val secondValue = 5
+    val thirdValue = firstValue - secondValue
+    println("thirdValue $thirdValue")
+    val lastname = "Zambrano"
+    val fullName = "$name $lastname"
+    println("fullName $fullName")
+}
