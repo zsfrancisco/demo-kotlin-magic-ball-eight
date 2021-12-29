42a8b379aeed6fd92578ec6370b3a2adf00de4f4
Francisco Zambrano <fzambran@bancolombia.com.co>
2021-12-28 22:22:15 -0500

    feat: control structure "if"
    Francisco Zambrano

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,9 @@
-const val PI = 3.1416
 fun main() {
-    println("Hello World!")
-    var money = 23
-    println("money - $money")
-    money = 10
-    println("money - $money")
-    val name = "Francisco"
-    println("name - $name")
-    println("PI - $PI")
-    val firstValue = 10
-    val secondValue = 5
-    val thirdValue = firstValue - secondValue
-    println("thirdValue $thirdValue")
-    val lastname = "Zambrano"
-    val fullName = "$name $lastname"
-    println("fullName $fullName")
+    val name = "Maria"
+    if (name.isNotEmpty()) println("name length is ${name.length}") else println("Error - name is empty")
+    val message =
+        if (name.length > 4) "your name is large"
+        else if (name.isEmpty()) "your name is empty"
+        else "your name is short"
+    println("message $message")
 }
