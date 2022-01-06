d9966db305c86e95c87db08ad7176c4232781224
Francisco Zambrano <fzambran@bancolombia.com.co>
2022-01-05 23:33:18 -0500

    feat: Elvis operator
    Francisco Zambrano

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,5 @@
 fun main() {
-    var name: String? = null
-    try {
-        throw NullPointerException("Error was happened FZ")
-    } catch (exception: NullPointerException) {
-        println("Error - $exception")
-    } finally {
-        println("finish")
-    }
-
-    val firstNumber = 10
-    val secondNumber = 0
-    val result: Int = try {
-        firstNumber / secondNumber
-    } catch (exception: Exception) {
-        0
-    }
-    println("Result is $result")
+    val name: String? = null
+    val charactersOfName: Int = name?.length ?: 0
+    println("charactersOfName is $charactersOfName")
 }
