715467719aaf40650d26624b68e7ad51067a2c2e
Francisco Zambrano <fzambran@bancolombia.com.co>
2022-01-05 22:51:37 -0500

    feat: control structure "while" and "do while"
    Francisco Zambrano

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,13 @@
 fun main() {
-    when ("Red") {
-        "yellow" -> println("Color is yellow")
-        "red", "Red" -> println("Color is red")
-        "blue" -> println("Color is blue")
-        else -> println("IDK what is the color")
+    var count = 10
+    while (count > 0) {
+        println("Count value is $count")
+        count--
     }
 
-    when (200) {
-        in 200..299 -> println("code is between 200 and 299")
-        in 400..500 -> println("code is between 400 and 500")
-        else -> println("IDK what is what range is the code")
-    }
-
-    val message = when (42) {
-        41, 42 -> "We have that shoe size"
-        43, 44 -> "We don't have much shoes with that size"
-        45 -> "We don't have that shoe size"
-        else -> "we don't that size shoe, only 41 - 45"
-    }
-
-    println("message $message")
+    do {
+        println("Generating random number")
+        val randomNumber = (0..100).random()
+        println("Random number is $randomNumber")
+    } while (randomNumber > 50)
 }
