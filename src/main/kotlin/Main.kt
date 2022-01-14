4dddd4d13c19a603199f85501aea06a57fa2dd8e
Francisco Zambrano <fzambran@bancolombia.com.co>
2022-01-13 23:14:30 -0500

    feat: functions and extend functions
    Francisco Zambrano

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,16 @@
 fun main() {
-    val vocalsRepeated = setOf("a", "e", "i", "o", "u", "a", "e")
-    println("vocals repeated are $vocalsRepeated")
+    printPhrase("Francisco Zambrano".randomCase())
+}
 
-    val ageRepeated = mutableSetOf(10, 11, 44, 5, 5, 10, 15, 20)
-    ageRepeated.add(45)
-    println("age repeated are $ageRepeated")
-    ageRepeated.remove(5)
-    println("age repeated are $ageRepeated")
-    val franciscoBirthdayYear = mutableSetOf(1, 9, 9, 7)
-    val franciscoBirthdayYearRepeated = mutableSetOf(9, 7, 1)
-    println("franciscoBirthdayYear and franciscoBirthdayYearRepeated are equals? ${franciscoBirthdayYear == franciscoBirthdayYearRepeated}")
+fun String.randomCase(): String {
+    val randomNumber = 0..99
+    val randomResult = randomNumber.random()
+    if (randomResult.rem(2) == 0) {
+        return this.lowercase()
+    }
+    return this.uppercase()
+}
 
-    println("first value is ${ageRepeated.first()}")
-    println("number > 10 ${ageRepeated.filter { age -> age > 10 }}")
+fun printPhrase(phrase: String) {
+    println(phrase)
 }
