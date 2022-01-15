fa786fea559bc1c94038a2b143be94a24d59c51b
Francisco Zambrano <fzambran@bancolombia.com.co>
2022-01-14 19:59:44 -0500

    feat: functions parameters by default
    Francisco Zambrano

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,9 @@
 fun main() {
-    printPhrase("Francisco Zambrano".randomCase())
+    printName(name = "Francisco", lastname = "Zambrano")
 }
 
-fun String.randomCase(): String {
-    val randomNumber = 0..99
-    val randomResult = randomNumber.random()
-    if (randomResult.rem(2) == 0) {
-        return this.lowercase()
-    }
-    return this.uppercase()
-}
-
-fun printPhrase(phrase: String) {
-    println(phrase)
+fun printName(
+    name: String, lastname: String, secondName: String = "", secondLastname: String = ""
+) {
+    println("Mi full name is $name $secondName $lastname $secondLastname")
 }
