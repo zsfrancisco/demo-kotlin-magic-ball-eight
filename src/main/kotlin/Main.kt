f0b29a29edb4e4ad2af88867f794b2458748bcce
Francisco Zambrano <fzambran@bancolombia.com.co>
2022-01-14 20:44:02 -0500

    feat: Apply
    Francisco Zambrano

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,19 @@
 fun main() {
     val mobiles = mutableListOf("Google Pixel 2XL", "Google Pixel 4a", "Xiaomi mi 9T")
-        .run {
+        .apply {
             removeIf { mobile -> mobile.contains("Google") }
-            this
         }
     println("mobiles are $mobiles")
+
+    var colors: MutableList<String>? = null
+    colors?.apply {
+        println("Our colors are $this")
+        println("Our colors size is $size")
+    }
+
+    colors = mutableListOf("Yellow", "Blue", "Red")
+    colors.apply {
+        println("Our colors are $this")
+        println("Our colors size is $size")
+    }
 }
