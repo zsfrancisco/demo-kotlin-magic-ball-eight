d7eacf2ab0c1b89e7a85e33abec4a0f1db08a7ff
Francisco Zambrano <fzambran@bancolombia.com.co>
2022-01-14 21:10:18 -0500

    feat: Also
    Francisco Zambrano

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,7 @@
 fun main() {
     val mobiles = mutableListOf("Google Pixel 2XL", "Google Pixel 4a", "Xiaomi mi 9T")
-        .apply {
-            removeIf { mobile -> mobile.contains("Google") }
-        }
+        .also { list ->
+            println("The original list value is $list")
+        }.asReversed()
     println("mobiles are $mobiles")
-
-    var colors: MutableList<String>? = null
-    colors?.apply {
-        println("Our colors are $this")
-        println("Our colors size is $size")
-    }
-
-    colors = mutableListOf("Yellow", "Blue", "Red")
-    colors.apply {
-        println("Our colors are $this")
-        println("Our colors size is $size")
-    }
 }
